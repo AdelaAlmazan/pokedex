9cbed8550b22a4c4434391de6c579c318f3677e9
AdelaAlmazan <64912000+AdelaAlmazan@users.noreply.github.com>
2022-11-11 12:07:08 -0600

    btn again

--- src/App.js
+++ src/App.js
@@ -24,24 +24,30 @@ const App = () => {
   const [viewAdd, setViewAdd] = useState(false);
   const [long, setLong] = useState(true);
   const getPokemon = async () => {
-    setNameBtn(true);
-    await axios
-      .get(Url)
-      .then((response) => {
-        let nueva = (response.data.results).filter(name => (name['name'].toLowerCase().includes(searchPokemon.toLowerCase())) ||
-          (name['name'].includes(searchPokemon.toLocaleUpperCase())))
-        setDataA(nueva)
-        console.log(dataA)
-        longL(nueva);
-      })
-      .catch((error) => {
-        console.log(error);
-        openNotificationWithIcon(
-          "error",
-          "ocurrio un error al cargar los registros",
-          "Error"
-        )
-      });
+    if (searchPokemon == "") {
+      setLong(false);
+      setNameBtn(true);
+    } else {
+      setNameBtn(true);
+      await axios
+        .get(Url)
+        .then((response) => {
+          let nueva = (response.data.results).filter(name => (name['name'].toLowerCase().includes(searchPokemon.toLowerCase())) ||
+            (name['name'].includes(searchPokemon.toLocaleUpperCase())))
+          setDataA(nueva)
+          console.log(dataA)
+          longL(nueva);
+        })
+        .catch((error) => {
+          console.log(error);
+          openNotificationWithIcon(
+            "error",
+            "ocurrio un error al cargar los registros",
+            "Error"
+          )
+        });
+    }
+
   };
 
   const longL = (n) => {
@@ -59,7 +65,6 @@ const App = () => {
 
 
   const handleName = (e) => {
-
     setSearchPokemon(e.target.value)
     console.log(searchPokemon)
   }
@@ -71,7 +76,6 @@ const App = () => {
     setViewAdd(false)
   }
 
-
   return (
     <div className="App">
       <div className="Header">
@@ -82,18 +86,13 @@ const App = () => {
 
         >Agregar</button> */}
       </div>
-
-
-
       <div className='main-pokedex'>
         <div className='div-search-pokedex'>
           <input
             onChange={handleName}
             placeholder='Pokemon'
             className="search-pokedex"
-            disabled={nameBtn}
-
-          >
+            disabled={nameBtn}>
           </input>
           <button
             onClick={nameBtn ? resetBtn : getPokemon}
@@ -102,12 +101,9 @@ const App = () => {
             {nameBtn ? 'Volver a intentarlo' : 'Buscar'}
           </button>
         </div>
-
-
         {
           long ?
             (dataA).map((total) => (
-
               <div>
                 <InfoPokemon
                   name={total.name}
@@ -119,17 +115,7 @@ const App = () => {
               className='div-noRecords'
             >No hay registros</div>
         }
-
-
-
-
-
       </div>
-
-      <AddPokemon
-        view={viewAdd}
-        ClosePokemon={ClosePokemon}
-      />
     </div>
   );
 }
